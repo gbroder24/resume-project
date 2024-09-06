82b14100b41e06cb0e3cfb18182beb49cf00dd64
Gary Broderick <garybroderick82@gmail.com>
2024-09-06 11:20:16 +0000

    Add emailjs script code snips to contact html and send email js files and include keys. Create function send mail

---
+++ assets/js/sendEmail.js
@@ -0,0 +1,17 @@
+// Enter your emailjs SERVICE_ID and TEMPLATE_ID
+function sendMail(contactForm) {
+    emailjs.send("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return false;  // To block from loading a new page
+};
